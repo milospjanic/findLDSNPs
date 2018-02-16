1a80a18bfbd15025bb299e3c160da62558a37a5f
Milos Pjanic <pjanic.milos@gmail.com>
2018-02-15 17:12:45 -0800

    Update findLDSNPs.sh

--- findLDSNPs.sh
+++ findLDSNPs.sh
@@ -41,6 +41,8 @@ then
 
 wget http://zzz.bwh.harvard.edu/plink/dist/plink-1.07-x86_64.zip
 unzip plink-1.07-x86_64.zip
+cp ./plink-1.07-x86_64/plink .
+chmod 755 plink
 
 fi
 
@@ -63,11 +65,11 @@ do
         while read line; do
                 set $line
                 tabix -fh ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz $1:`expr $2 - 10000`-`expr $2 + 10000` > genotypes_chr$1_$4.vcf
-                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 $LD_METRIC --ld-window-r2 $LD_SCORE --out ld_results_${filename}
+                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 dprime --ld-window-r2 0.5 --out ld_results_${filename}
         cat ld_results_${filename}.ld >> ${filename}.leadplusLD
 
         rm genotypes_chr$1_$4.vcf
-        rm ld_results_${filename}.*
+        rm ld_results_${filename}.ld
         done < $file
 done
 
@@ -82,11 +84,11 @@ do
         while read line; do
                 set $line
                 tabix -fh ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz $1:`expr $2 - 10000`-`expr $2 + 10000` > genotypes_chr$1_$4.vcf
-                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 $LD_METRIC --ld-window-r2 $LD_SCORE --out ld_results_${filename}.X
+                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 dprime --ld-window-r2 0.5 --out ld_results_${filename}.X
         cat ld_results_${filename}.X.ld >> ${filename}.leadplusLD
 
         rm genotypes_chr$1_$4.vcf
-        rm ld_results_${filename}.*
+        rm ld_results_${filename}.X.ld
         done < $file
 done
 
@@ -97,14 +99,28 @@ do
         while read line; do
                 set $line                
 		tabix -fh ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrY.phase3_integrated_v2a.20130502.genotypes.vcf.gz $1:`expr $2 - 10000`-`expr $2 + 10000` > genotypes_chr$1_$4.vcf
-                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 $LD_METRIC --ld-window-r2 $LD_SCORE --out ld_results_${filename}.Y
+                ./plink --vcf genotypes_chr$1_$4.vcf --ld-snp $4 --r2 dprime --ld-window-r2 0.5 --out ld_results_${filename}.Y
         cat ld_results_${filename}.Y.ld >> ${filename}.leadplusLD
 
         rm genotypes_chr$1_$4.vcf
-        rm ld_results_${filename}.*
+        rm ld_results_${filename}.Y.ld
         done < $file
 done
 
 
-tabsep $1.leadplusLD
-cut -f7 $1.leadplusLD | grep -v SNP > $1.leadplusLD.cut
+tabsep $SNPS.leadplusLD
+grep -v CHR $SNPS.leadplusLD > $SNPS.leadplusLD.cut
+
+if [ "$LD_METRIC" = "R2" ]
+then
+awk '{if ($7>"'"$LD_SCORE"'") printf $0"\n"}' $SNPS.leadplusLD.cut > $SNPS.leadplusLD.cut.select
+fi
+
+if [ "$LD_METRIC" = "dprime" ]
+then
+awk '{if ($8> "'"$LD_SCORE"'") printf $0"\n"}' $SNPS.leadplusLD.cut > $SNPS.leadplusLD.cut.select
+fi
+
+tabsep $SNPS.leadplusLD.cut.select
+cut -f7 $SNPS.leadplusLD.cut.select > $SNPS.leadplusLD.final.list
+
